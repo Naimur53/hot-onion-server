069f4cde70e184e27b6da6bc789ae42ed8a86549
Naimur53 <naimurrhman53@gmail.com>
2021-10-27 12:05:33 +0600

    finished

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const ObjectId = require('mongodb').ObjectId
+
+
+
+// middle ware 
+app.use(cors());
+app.use(express.json());
+
+
+//mongo db
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.icikx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+
+        const database = client.db('foodShop');
+        const productCollection = database.collection('products');
+
+        app.get('/products', async (req, res) => {
+            console.log('hitting database');
+            const cursor = await productCollection.find({});
+            const result = await cursor.toArray();
+
+            console.log(result);
+            res.json(result);
+        })
+        //get api
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) };
+            const cursor = await productCollection.findOne(query);
+            console.log(cursor);
+            res.json(cursor)
+        })
+
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
+
+//default 
+app.get('/', (req, res) => {
+    console.log('hi');
+    res.send('server running');
+
+})
+
+//listen
+app.listen(port, () => {
+    console.log('sever is running at ', port);
+})
